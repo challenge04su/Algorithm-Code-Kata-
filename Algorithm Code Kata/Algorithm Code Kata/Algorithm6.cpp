42615cf529475a5efa64bb02e5646e4c92f09e43
challenge04su <challenge04su@gmail.com>
2025-12-23 09:12:43 +0900

    Day02
    6. 두 수의 합

---
+++ Algorithm Code Kata/Algorithm Code Kata/Algorithm6.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    if (num1 >= -50000 && num1 <= 50000 && num2 >= -50000 && num2 <= 50000)
+    {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
+
+int main()
+{
+    solution(2, 3);
+    solution(100, 2);
+}
