15c1dc0508ec586c622cbe75ececb1bc7855c96d
challenge04su <challenge04su@gmail.com>
2026-01-05 09:08:28 +0900

    Day09
    25. 나누어 떨어지는 숫자

---
+++ Algorithm Code Kata(08~14)/Algorithm Code Kata/Algorithm25.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor)
+{
+    vector<int> answer;
+
+    for (int n : arr)
+    {
+        if (n % divisor == 0)
+        {
+            answer.push_back(n);
+        }
+    }
+    if (answer.empty())
+    {
+        answer.push_back(-1);
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
