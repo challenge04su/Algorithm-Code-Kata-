c8e3db2a49400b5ad32058785ba2e2b32f9fd4a9
challenge04su <challenge04su@gmail.com>
2026-01-13 09:14:56 +0900

    Day15
    43. 크기가 작은 부분

---
+++ Algorithm Code Kata(15~21)/Algorithm Code Kata(15~21)/Algorithm43.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string t, string p)
+{
+    int answer = 0;
+
+    for (int i = 0; i <= t.length() - p.length(); i++)
+    {
+        string s = t.substr(i, p.length());
+
+        if (s <= p)
+        {
+            answer++;
+        }
+    }
+    return answer;
+}
