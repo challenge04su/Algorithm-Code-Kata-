5602aee21594e423250f2a370662b3b1123fd60c
josukyoung <challenge04su@gmail.com>
2026-04-20 09:14:55 +0900

    Day77
    Basic_Day14_05
    : 수열과 구간 쿼리 1

---
+++ Algorithm Code Kata(71~77)/Basic_Day14_05.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries)
+{
+    for (auto& q : queries)
+    {
+        int s = q[0];
+        int e = q[1];
+
+        for (int i = s; i <= e; i++)
+        {
+            arr[i] += 1;
+        }
+    }
+    return arr;
+}
